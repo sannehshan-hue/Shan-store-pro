823f7a215be78079878deb77c4dbac62f1260905
sannehshan-hue <sannehshan@gmail.com>
2026-05-09 00:06:14 +0300

    Create installer.sh

---
+++ installer.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "🚀 Installing Cyberx Hub Enterprise"
+
+mkdir cyberx-hub
+cd cyberx-hub
+
+mkdir frontend backend database
+mkdir backend/routes
+mkdir frontend/assets
+
+echo "✅ Project folders created"
